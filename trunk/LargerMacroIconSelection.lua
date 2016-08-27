2acf9aa297ae28f587efdc7228e90cc3fc4dffec
Ketho <Ketho@a1e91e8a-1c29-4b0b-935e-9a8a852029ff>
2016-08-27 06:02:16 +0000

    Add search functionality for Macro and Equipment Manager icons

--- trunk/LargerMacroIconSelection.lua
+++ trunk/LargerMacroIconSelection.lua
@@ -1,7 +1,7 @@
 --[[
-LargerMacroIconSelection v1.0.9
-6th July 2016
-Copyright (C) 2016 Xinhuan
+LargerMacroIconSelection v1.1.0
+27th August 2016
+Copyright (C) 2016 Xinhuan, Ketho
 
 Shows you a much bigger macro icon selection frame instead of the
 standard 5x4 one.
@@ -24,15 +24,15 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
--- Note: learned a lot from Expanded Macro Selection (EMS)
+-- Note: learned a lot from Expanded Macro Selection (EMS) and Macro Toolkit
+local LMIS = CreateFrame("Frame", "LargerMacroIconSelection")
+local LibAIS = LibStub("LibAdvancedIconSelector-1.0-LMIS")
+
 local NAME, S = ...
 local L = S.L -- Localization
+local FileData
 local db
-
-local defaults = {
-	width = 10,
-	height = 10,
-}
+LMIS.S = S -- for LibAIS to read FileData
 
 -- Local constants
 local ICONS_PER_ROW, ICON_ROWS, ICONS_SHOWN
@@ -40,24 +40,47 @@ local previousbuttons = 0
 
 local popup_regions, sf_regions = {}, {}
 local origSize, origNum = {}, {}
+local active = {}
 
+local searchObject, activeSearch
+local searchIcons = {}
+
+local GB_ICON_FILENAMES
+
+-- put this in global namespace for consistency/simplicity
+function GetGuildBankIconInfo(index)
+	return GB_ICON_FILENAMES[index]
+end
+
+local defaults = {
+	width = 10,
+	height = 10,
+}
+
+-- sometimes we need the variable name instead of value/pointer
 local frames = {
 	MacroPopupScrollFrame = function() return {
 			icons_per_row = "NUM_ICONS_PER_ROW", -- 5
 			icon_rows = "NUM_ICON_ROWS", -- 4
 			icons_shown = "NUM_MACRO_ICONS_SHOWN", -- 20
+			icon_row_height = MACRO_ICON_ROW_HEIGHT, -- 36
+			geticoninfo = "GetSpellorMacroIconInfo",
 			button = "MacroPopupButton",
 			template = "MacroPopupButtonTemplate",
-			update = MacroPopupFrame_Update,
+			update = "MacroPopupFrame_Update",
+			okaybutton = MacroPopupOkayButton,
 		}
 	end,
 	GearManagerDialogPopupScrollFrame = function() return {
 			icons_per_row = "NUM_GEARSET_ICONS_PER_ROW", -- 5
 			icon_rows = "NUM_GEARSET_ICON_ROWS", -- 3
 			icons_shown = "NUM_GEARSET_ICONS_SHOWN", -- 15
+			icon_row_height = GEARSET_ICON_ROW_HEIGHT, -- 36
+			geticoninfo = "GetEquipmentSetIconInfo",
 			button = "GearManagerDialogPopupButton",
 			template = "GearSetPopupButtonTemplate",
-			update = GearManagerDialogPopup_Update,
+			update = "GearManagerDialogPopup_Update",
+			okaybutton = GearManagerDialogPopupOkay,
 			topcoords = 212/256, -- Different TexCoord
 		}
 	end,
@@ -65,15 +88,58 @@ local frames = {
 			icons_per_row = "NUM_GUILDBANK_ICONS_PER_ROW", -- 4
 			icon_rows = "NUM_GUILDBANK_ICON_ROWS", -- 4
 			icons_shown = "NUM_GUILDBANK_ICONS_SHOWN", -- 16
+			icon_row_height = GUILDBANK_ICON_ROW_HEIGHT, -- 36
+			geticoninfo = "GetGuildBankIconInfo", -- custom; since its streamlined into the update func
 			button = "GuildBankPopupButton",
 			template = "GuildBankPopupButtonTemplate",
-			update = GuildBankPopupFrame_Update,
+			update = "GuildBankPopupFrame_Update",
+			okaybutton = GuildBankPopupOkayButton,
 			extrawidth = 8, -- GuildBank is slightly thinner
 		}
 	end,
 }
 
-local f = CreateFrame("Frame")
+local function LoadFileData()
+	if not FileData then
+		local loaded, reason = LoadAddOn("LargerMacroIconSelectionData")
+		
+		if not loaded and reason == "DISABLED" then
+			EnableAddOn("LargerMacroIconSelectionData", true)
+			LoadAddOn("LargerMacroIconSelectionData")
+		end
+		FileData = S.FileData
+	end
+end
+
+local function InitSearch()
+	if not searchObject then
+		searchObject = LibAIS:CreateSearch()
+		
+		searchObject:SetScript("OnSearchStarted", function(self)
+			wipe(searchIcons)
+		end)
+		
+		searchObject:SetScript("OnSearchResultAdded", function(self, texture, _, _, _, FileDataID)
+			tinsert(searchIcons, FileDataID)
+		end)
+		
+		searchObject:SetScript("OnSearchComplete", function(self)
+			-- The scrollframe is hidden when there are not enough icons, use the popupframe instead
+			if activeSearch and activeSearch:GetParent():IsShown() then
+				_G[frames[activeSearch].update]()
+			end
+			
+			local eb = activeSearch:GetParent().SearchBox
+			if not next(searchIcons) then
+				eb:SetTextColor(1, 0, 0)
+			else
+				eb:SetTextColor(1, 1, 1)
+			end
+		end)
+	end
+end
+
+local f = LargerMacroIconSelection
 
 function f:OnEvent(event, addon)
 	if addon == NAME then
@@ -86,15 +152,18 @@ function f:OnEvent(event, addon)
 		
 		self:Initialize(GearManagerDialogPopupScrollFrame)
 		
-		if IsAddOnLoaded("Blizzard_MacroUI") then -- Someone else made it load before us
+		-- Someone else made it load before us
+		if IsAddOnLoaded("Blizzard_MacroUI") then
 			self:Initialize(MacroPopupScrollFrame)
 		end
 		
 	elseif addon == "Blizzard_MacroUI" then
 		self:Initialize(MacroPopupScrollFrame)
+		
 	elseif addon == "Blizzard_GuildBankUI" then
-		if not IsGuildLeader() then return end -- No access to guild bank tabs
-		self:Initialize(GuildBankPopupScrollFrame)
+		if IsGuildLeader() then
+			self:Initialize(GuildBankPopupScrollFrame)
+		end
 	end
 end
 
@@ -104,47 +173,129 @@ f:SetScript("OnEvent", f.OnEvent)
 function f:Initialize(sf)
 	local popup = sf:GetParent()
 	
-	-- Get frame specific data
-	frames[sf] = frames[sf:GetName()]()
-	
-	-- Group the anonymous textures into a table before adding more regions
-	popup_regions[sf] = {popup:GetRegions()}
-	sf_regions[sf] = {sf:GetRegions()}
-	
-	-- Get original dimensions
-	origSize[sf] = {
-		width = popup:GetWidth(),
-		height = popup:GetHeight(),
-		sfwidth = sf:GetWidth(),
-		sfheight = sf:GetHeight(),
-	}
-	
-	-- Get original amount of rows and columns
-	origNum[sf] = {
-		icons_per_row = _G[frames[sf].icons_per_row],
-		icon_rows = _G[frames[sf].icon_rows],
-		icons_shown =  _G[frames[sf].icons_shown],
-	}
-	
-	-- Create extra background textures
-	for i = 1, 6 do
-		popup["largertexture"..i] = popup:CreateTexture(nil, "BACKGROUND")
-	end
-	sf.largertexture1 = sf:CreateTexture(nil, "BACKGROUND") -- Scrollframe texture
-	
-	-- Make movable
-	popup:SetMovable(true)
-	popup:SetClampedToScreen(true)
-	popup:SetFrameStrata("HIGH") -- up from "Medium", GearManager fix
-	
-	popup:EnableMouse(true) --  GearManager fix
-	popup:RegisterForDrag("LeftButton")
-	popup:SetScript("OnDragStart", popup.StartMoving)
-	popup:SetScript("OnDragStop", popup.StopMovingOrSizing)
-	
-	-- Add buttons, move textures
-	self:UpdateButtons(sf)
-	self:UpdateTextures(sf)
+	-- Only initialize when the popupframe shows
+	popup:HookScript("OnShow", function()
+		if active[sf] then return end
+		
+		-- Only load file data when needed to save memory space
+		LoadFileData()
+		InitSearch()
+		
+		-- Get frame specific data
+		frames[sf] = frames[sf:GetName()]()
+		
+		-- Group the anonymous textures into a table before adding more regions
+		popup_regions[sf] = {popup:GetRegions()}
+		sf_regions[sf] = {sf:GetRegions()}
+		
+		-- Get original dimensions
+		origSize[sf] = {
+			width = popup:GetWidth(),
+			height = popup:GetHeight(),
+			sfwidth = sf:GetWidth(),
+			sfheight = sf:GetHeight(),
+		}
+		
+		-- Get original amount of rows and columns
+		origNum[sf] = {
+			icons_per_row = _G[frames[sf].icons_per_row],
+			icon_rows = _G[frames[sf].icon_rows],
+			icons_shown =  _G[frames[sf].icons_shown],
+		}
+		
+		-- Create extra background textures
+		for i = 1, 6 do
+			popup["largertexture"..i] = popup:CreateTexture(nil, "BACKGROUND")
+		end
+		sf.largertexture1 = sf:CreateTexture(nil, "BACKGROUND") -- Scrollframe texture
+		
+		-- Movable
+		popup:SetMovable(true)
+		popup:SetClampedToScreen(true)
+		popup:SetFrameStrata("HIGH") -- up from "Medium", GearManager was hidden behind stuff
+		
+		popup:EnableMouse(true) -- GearManager not mouse enabled
+		popup:RegisterForDrag("LeftButton")
+		popup:SetScript("OnDragStart", popup.StartMoving)
+		popup:SetScript("OnDragStop", popup.StopMovingOrSizing)
+		
+		-- Update GameTooltip when scrollling
+		sf:HookScript("OnMouseWheel", function()
+			local focus = GetMouseFocus()
+			if focus and focus:GetObjectType() == "CheckButton" then
+				focus:GetScript("OnEnter")(focus)
+			end
+		end)
+		
+		if popup == GuildBankPopupFrame then
+			-- Can not make the Guild Bank support icon search; at least support GameTooltip info
+			GB_ICON_FILENAMES = {}
+			GB_ICON_FILENAMES[1] = "INV_MISC_QUESTIONMARK"
+
+			GetLooseMacroItemIcons(GB_ICON_FILENAMES)
+			GetLooseMacroIcons(GB_ICON_FILENAMES)
+			GetMacroItemIcons(GB_ICON_FILENAMES)
+			GetMacroIcons(GB_ICON_FILENAMES)
+		else
+			local searchLabel = popup:CreateFontString()
+			searchLabel:SetFontObject("GameFontNormal")
+			searchLabel:SetText(SEARCH..":")
+			searchLabel:SetPoint("BOTTOMLEFT", 20, 19)
+			
+			local eb = CreateFrame("EditBox", "$parentEditBox", popup, "InputBoxTemplate")
+			eb:SetPoint("LEFT", searchLabel, "RIGHT", 7, -2)
+			eb:SetPoint("RIGHT", frames[sf].okaybutton, "LEFT", 0, 0)
+			eb:SetHeight(15)
+			popup.SearchBox = eb
+			
+			eb:SetScript("OnTextChanged", function(self, userInput)
+				if userInput then
+					searchObject:SetSearchParameter(self:GetText())
+					activeSearch = sf
+				end
+			end)
+			
+			eb:SetScript("OnEnterPressed", function(self)
+				self:ClearFocus()
+			end)
+			
+			popup:HookScript("OnHide", function()
+				-- Stop search and reset filters
+				searchObject:Stop()
+				activeSearch = nil
+				eb:SetText("")
+				wipe(searchIcons)
+			end)
+
+			-- Update scroll bar for the filtered icons
+			hooksecurefunc(frames[sf].update, function()
+				if #searchIcons > 0 then
+					FauxScrollFrame_Update(sf, ceil(#searchIcons / ICONS_PER_ROW), ICON_ROWS, frames[sf].icon_row_height)
+				end
+			end)
+			
+			local isGearManager = (popup == GearManagerDialogPopup)
+			
+			-- Prehook GetIconInfo for search functionality 
+			local oldGetIconInfo = _G[frames[sf].geticoninfo]
+			_G[frames[sf].geticoninfo] = function(index)
+				if #searchIcons > 0 then
+					-- GearManager does not cope well with nil values
+					return searchIcons[index] or isGearManager and "INV_MISC_QUESTIONMARK"
+				else
+					return oldGetIconInfo(index)
+				end
+			end
+		end
+		
+		-- Add buttons, move textures
+		self:UpdateButtons(sf)
+		self:UpdateTextures(sf)
+		-- only initialized with OnShow, update again
+		_G[frames[sf].update]()
+		
+		active[sf] = true
+	end)
 end
 
 -- Initialization that should be called when the width/height values change
@@ -159,8 +310,9 @@ function f:UpdateButtons(sf)
 	_G[frame.icon_rows] = ICON_ROWS
 	_G[frame.icons_shown] = ICONS_SHOWN
 	
-	-- Add buttons
-	for i = 2, ICONS_SHOWN do
+	--print("UpdateButtons", ICONS_PER_ROW, ICON_ROWS)
+	
+	for i = 1, ICONS_SHOWN do
 		local b = _G[button..i]
 		
 		if not b then -- Create button
@@ -173,18 +325,36 @@ function f:UpdateButtons(sf)
 		end
 		
 		-- Position buttons
-		b:ClearAllPoints()
-		if i % ICONS_PER_ROW == 1 then
-			b:SetPoint("TOPLEFT", _G[button..(i-ICONS_PER_ROW)], "BOTTOMLEFT", 0, -8)
-		else
-			b:SetPoint("LEFT", _G[button..i-1], "RIGHT", 10, 0)
+		if i > 1 then
+			b:ClearAllPoints()
+			if i % ICONS_PER_ROW == 1 then
+				b:SetPoint("TOPLEFT", _G[button..(i-ICONS_PER_ROW)], "BOTTOMLEFT", 0, -8)
+			else
+				b:SetPoint("LEFT", _G[button..i-1], "RIGHT", 10, 0)
+			end
 		end
+		
+		-- Show icon information
+		b:SetScript("OnEnter", function(self)
+			GameTooltip:SetOwner(self, "ANCHOR_TOPLEFT")
+			
+			local id = FauxScrollFrame_GetOffset(sf)*ICONS_PER_ROW + self:GetID()
+			GameTooltip:AddLine(id)
+			
+			local texture = _G[frame.geticoninfo](id)
+			GameTooltip:AddLine(type(texture) == "number" and FileData[texture] or texture, 1, 1, 1)
+			GameTooltip:Show()
+		end)
+		
+		b:SetScript("OnLeave", function(self)
+			GameTooltip:Hide()
+		end)
 	end
 	
 	-- Hide any superfluous buttons
 	for i = ICONS_SHOWN + 1, previousbuttons do
 		local b = _G[button..i]
-		if b then -- Sanity check
+		if b then
 			b:Hide()
 		end
 	end
@@ -205,7 +375,7 @@ function f:UpdateTextures(sf)
 	sf:SetWidth(size.sfwidth + extrawidth)
 	sf:SetHeight(size.sfheight + extraheight)
 	
-	local isGuildBank = (popup == GuildBankPopupFrame) -- GuildBank fix
+	local isGuildBank = (popup == GuildBankPopupFrame)
 	
 	-- Reposition the unnamed textures, as well as init
 	-- the extra ones to cover up the extra areas
@@ -294,14 +464,6 @@ function f:UpdateTextures(sf)
 	end
 end
 
-local function MaxSize(v, maxValue, str)
-	if v > maxValue then
-		print(format("%s |cffFFFF00%d|r is too large!", str, v))
-		v = maxValue
-	end
-	return v
-end
-
 -- Slash commands
 for i, v in ipairs({"lmis", "largermacro", "largermacroiconselection"}) do
 	_G["SLASH_LARGERMACROICONSELECTION"..i] = "/"..v
@@ -312,31 +474,27 @@ SlashCmdList.LARGERMACROICONSELECTION = function(msg)
 	width = floor(tonumber(width) or 0)
 	height = floor(tonumber(height) or 0)
 	
-	if width >= 5 and height >= 4 then -- sanitize
+	if width >= 5 and height >= 4 then
 		-- Avoid outgrowing the screen (1920x1080, normal UI Scale)
-		width = MaxSize(width, 40, "Width")
-		height = MaxSize(height, 21, "Height")
-		
+		width = min(width, 40)
+		height = min(height, 21)
 		print(L.SETTING_VALUES:format(width, height))
 		
-		-- Update db
 		db.width = width
 		db.height = height
 		
-		-- Update upvalues
-		previousbuttons = ICONS_SHOWN or 0 -- Remember last amount
+		previousbuttons = ICONS_SHOWN or 0
 		ICONS_PER_ROW = width
 		ICON_ROWS = height
 		ICONS_SHOWN = ICONS_PER_ROW * ICON_ROWS
 		
-		-- Update buttons
-		for k in pairs(frames) do
-			local sf = _G[k] -- Get the frames, the function types on the same table are skipped
-			if sf then
+		for k, v in pairs(frames) do
+			if type(v) == "table" then
+				local sf = k
 				f:UpdateButtons(sf)
 				f:UpdateTextures(sf)
-				if sf:IsVisible() then -- force update
-					frames[sf].update()
+				if sf:GetParent():IsShown() then
+					_G[frames[sf].update]()
 				end
 			end
 		end
