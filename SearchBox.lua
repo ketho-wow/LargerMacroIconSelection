945aa8e201ea53c471619c7b996ff11ab4f44d8b
Ketho <sghs17@outlook.com>
2022-11-20 23:15:33 +0100

    Initial rewrite for Dragonflight

--- SearchBox.lua
+++ SearchBox.lua
@@ -3,80 +3,103 @@ local LMIS = LargerMacroIconSelection
 local LibAIS = LibStub("LibAdvancedIconSelector-1.0-LMIS")
 
 LMIS.searchIcons = {}
-
 local LibAIS_options = {
 	sectionOrder = {"FileDataIcons"},
 }
 
+function LMIS:CreateSearchBox(popup)
+	local sb = CreateFrame("EditBox", "$parentSearchBox", popup, "InputBoxTemplate")
+	sb:SetPoint("BOTTOMLEFT", 74, 15)
+	sb:SetPoint("RIGHT", popup.BorderBox.OkayButton, "LEFT", 0, 0)
+	sb:SetHeight(15)
+	sb:SetFrameLevel(popup.BorderBox:GetFrameLevel()+1)
+	sb.searchLabel = sb:CreateFontString()
+	sb.searchLabel:SetPoint("RIGHT", sb, "LEFT", -8, 0)
+	sb.searchLabel:SetFontObject("GameFontNormal")
+	sb.searchLabel:SetText(SEARCH..":")
+	sb.linkLabel = sb:CreateFontString()
+	sb.linkLabel:SetPoint("RIGHT", popup.BorderBox.OkayButton, "LEFT", -5, -1)
+	sb.linkLabel:SetFontObject("GameFontNormal")
+	sb.linkLabel:SetTextColor(.62, .62, .62)
+	sb:SetScript("OnTextChanged", self.SearchBox_OnTextChanged)
+	sb:SetScript("OnEnterPressed", function()
+		sb:ClearFocus()
+	end)
+	sb.spinner = CreateFrame("Frame", nil, sb, "LoadingSpinnerTemplate")
+	sb.spinner:SetPoint("RIGHT", -4, -1)
+	sb.spinner:SetSize(20, 20)
+	sb.spinner:Hide()
+	popup:HookScript("OnHide", function()
+		self:ClearSearch(popup)
+		sb:SetText("")
+		sb:SetTextColor(1, 1, 1)
+		sb.linkLabel:SetText()
+	end)
+	-- support shift-clicking links to the search box
+	hooksecurefunc("ChatEdit_InsertLink", function(text)
+		if text and sb:IsVisible() then
+			sb:SetText(strmatch(text, "H(%l+:%d+)") or "")
+			RunNextFrame(function() StackSplitFrame:Hide() end)
+		end
+	end)
+	sb.popup = popup
+	popup.SearchBox = sb
+end
+
 function LMIS:InitSearch()
 	if not self.searchObject then
 		self.searchObject = LibAIS:CreateSearch(LibAIS_options)
-		self.searchObject:SetScript("OnSearchStarted", function(_self)
+		self.searchObject:SetScript("OnSearchStarted", function()
 			wipe(self.searchIcons)
+			self.activeSearch.SearchBox.spinner:Show()
 		end)
 		self.searchObject:SetScript("OnSearchResultAdded", function(_self, texture, _, _, _, fdid)
 			tinsert(self.searchIcons, fdid)
 		end)
-		self.searchObject:SetScript("OnSearchComplete", function(_self)
-			if self.activeSearch then
-				local info = self.activeSearch
-				local popup = info.popup
+		self.searchObject:SetScript("OnSearchComplete", function()
+			local popup = self.activeSearch
+			if popup then
 				if not popup:IsShown() then return end
-				-- in 7.2 GB_ICON_FILENAMES was made accessible (thanks!), but as a table instead of function
-				if popup == GuildBankPopupFrame then
-					GB_ICON_FILENAMES = self.searchIcons
-				end
 				if #self.searchIcons == 0 then
 					popup.SearchBox:SetTextColor(1, 0, 0)
 				else
 					popup.SearchBox:SetTextColor(1, 1, 1)
+					self:SetSearchData(popup)
+					self:UpdateSearchPopup(popup)
 				end
-				self:UpdateSearchPopup(info)
 				self.activeSearch = nil
+				popup.SearchBox.spinner:Hide()
 			end
 		end)
 	end
 end
 
-function LMIS:ClearSearch(info)
+function LMIS:ClearSearch(popup)
+	popup.SearchBox.spinner:Hide()
 	self.activeSearch = nil
 	wipe(self.searchIcons)
 	self.searchObject:Stop()
-	if info.popup == GuildBankPopupFrame then
-		GB_ICON_FILENAMES = CopyTable(LMIS.GB_ORIGINAL)
-	end
 end
 
-function LMIS:UpdateSearchPopup(info)
-	local popup = info.popup
-	popup.selectedIcon = nil
-	info.sf:SetVerticalScroll(0)
-	if popup == GuildBankPopupFrame then
-		self.frameInfo[popup].update(popup)
-	else
-		local text = info.editbox:GetText()
-		_G[info.update]()
-		info.editbox:SetText(text) -- updating clears the EditBox; remember the macro name
-	end
-	if popup == GearManagerDialogPopup and #self.searchIcons > 0 then
-		self:UpdateButtons(info, #self.searchIcons)
-	end
-	self:RefreshMouseFocus()
-	-- the Blizzard UI remembers the ScrollFrame offset id instead
-	-- of the previously selected icon when starting a new search
-	if popup == MacroPopupFrame then
-		MacroFrameSelectedMacroButtonIcon:SetTexture("Interface\\Icons\\INV_MISC_QUESTIONMARK")
-	end
+function LMIS:SetSearchData(popup)
+	wipe(popup.iconDataProvider.extraIcons)
+	popup.iconDataProvider:SetIconData(LMIS.searchIcons)
+end
+
+function LMIS:UpdateSearchPopup(popup)
+	local text = popup.BorderBox.IconSelectorEditBox:GetText()
+	popup.Update(popup) -- updating clears the EditBox
+	popup.BorderBox.IconSelectorEditBox:SetText(text)
 end
 
 function LMIS.SearchBox_OnTextChanged(sb, userInput)
-	local info = sb.info
+	local popup = sb.popup
 	local text = sb:GetText()
 	local isNumber = tonumber(text)
 	if isNumber or strfind(text, "[:=]") then -- search by spell/item/achievement id
 		local link, id = text:lower():match("(%a+)[:=](%d+)")
 		local linkSearch
-		LMIS:ClearSearch(info)
+		LMIS:ClearSearch(popup)
 		if isNumber or link == "filedata" and id then
 			linkSearch = S.FileData[isNumber or tonumber(id)]
 		elseif link == "spell" and id then
@@ -87,29 +110,27 @@ function LMIS.SearchBox_OnTextChanged(sb, userInput)
 			linkSearch = S.FileData[select(10, GetAchievementInfo(id))]
 		end
 		if linkSearch then
-			LMIS.activeSearch = info
+			LMIS.activeSearch = popup
 			LMIS.searchIcons[1] = linkSearch
 			sb:SetTextColor(1, 1, 1)
 			sb.linkLabel:SetText(linkSearch)
-			if info.popup == GuildBankPopupFrame then
-				GB_ICON_FILENAMES = {linkSearch}
-			end
+			LMIS:SetSearchData(popup)
+			LMIS:UpdateSearchPopup(popup)
 		else
 			LMIS.searchIcons[1] = "INV_MISC_QUESTIONMARK"
 			sb:SetTextColor(1, 0, 0)
 			sb.linkLabel:SetText()
 		end
-		LMIS:UpdateSearchPopup(info)
-		_G[info.buttons.."1"]:Click()
 	else
 		sb:SetTextColor(1, 1, 1)
 		sb.linkLabel:SetText()
 		if #text > 0 then -- search by texture name
 			LMIS.searchObject:SetSearchParameter(text)
-			LMIS.activeSearch = info
+			LMIS.activeSearch = popup
 		else
-			LMIS:ClearSearch(info)
-			LMIS:UpdateSearchPopup(info)
+			LMIS:ClearSearch(popup)
+			LMIS:UpdateSearchPopup(popup)
+			LMIS:UpdateIconSelector(popup)
 		end
 	end
 end
