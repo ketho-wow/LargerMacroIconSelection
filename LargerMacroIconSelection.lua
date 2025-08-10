8929bbc19379e98c402fd236635eab6f3eea71ad
Ketho <sghs17@outlook.com>
2025-08-10 03:40:33 +0200

    Added support for Baganator bank Fixed error by making Macro window no longer movable

--- LargerMacroIconSelection.lua
+++ LargerMacroIconSelection.lua
@@ -27,7 +27,7 @@ function LMIS:LoadFileData(addon)
 	end
 end
 
-function LMIS:OnEvent(event, addon)
+function LMIS:ADDON_LOADED(event, addon)
 	if addon == "LargerMacroIconSelection" then
 		if not self.isVanilla then
 			self:Initialize(GearManagerPopupFrame)
@@ -52,9 +52,23 @@ function LMIS:OnEvent(event, addon)
 		EventUtil.ContinueOnAddOnLoaded("Blizzard_GuildBankUI", function()
 			self:Initialize(GuildBankPopupFrame)
 		end)
+		self:UnregisterEvent("ADDON_LOADED")
 	end
 end
 
+function LMIS:PLAYER_LOGIN(event)
+	if self.isMainline then
+		EventUtil.ContinueOnAddOnLoaded("Baganator", function()
+			self:Initialize(Baganator_CategoryViewBankViewFrameblizzard.Character.TabSettingsMenu)
+		end)
+	end
+end
+
+function LMIS:OnEvent(event, ...)
+	self[event](self, event, ...)
+end
+
+LMIS:RegisterEvent("PLAYER_LOGIN")
 LMIS:RegisterEvent("ADDON_LOADED")
 LMIS:SetScript("OnEvent", LMIS.OnEvent)
 
@@ -73,36 +87,43 @@ function LMIS:Initialize(popup)
 		end)
 		self:LoadFileData("LargerMacroIconSelectionData")
 		self:InitSearch()
-		-- movable
-		popup:SetMovable(true)
-		popup:SetClampedToScreen(true)
-		popup:RegisterForDrag("LeftButton")
-		popup:SetScript("OnDragStart", function()
-			-- StartMoving makes it user placed which would make the macro popup remember its position and error
-			popup:StartMoving()
-			popup:SetUserPlaced(false)
-		end)
-		popup:SetScript("OnDragStop", popup.StopMovingOrSizing)
-		-- searchbox
-		self:CreateSearchBox(popup)
-		-- icon tooltip
-		for _, btn in pairs(popup.IconSelector.ScrollBox:GetFrames()) do
-			btn:HookScript("OnEnter", LMIS.ShowTooltip)
-			btn:HookScript("OnLeave", LMIS.GameTooltip_Hide)
+		if popup ~= MacroPopupFrame then -- errors after being moved and reshown
+			self:SetFrameMovable(popup)
 		end
-		popup.BorderBox.SelectedIconArea.SelectedIconButton:HookScript("OnEnter", function(btn)
-			local fileid = btn:GetIconTexture()
-			self:SetIconTooltip(btn, function()
-				if fileid ~= 134400 then -- inv_misc_questionmark
-					GameTooltip:AddLine(format("|cff71D5FF%s|r", fileid))
-					if S.FileData[fileid] then
-						GameTooltip:AddLine(S.FileData[fileid], 1, 1, 1)
-					end
-				end
-			end)
-		end)
+		self:CreateSearchBox(popup)
+		self:CreateIconTooltip(popup)
 		-- need all buttons created and hooked before changing the data provider
 		self:UpdateIconSelector(popup)
+	end)
+end
+
+function LMIS:SetFrameMovable(popup)
+	popup:SetMovable(true)
+	popup:SetClampedToScreen(true)
+	popup:RegisterForDrag("LeftButton")
+	popup:SetScript("OnDragStart", function()
+		-- StartMoving makes it user placed which would make the macro popup remember its position and error
+		popup:StartMoving()
+		popup:SetUserPlaced(false)
+	end)
+	popup:SetScript("OnDragStop", popup.StopMovingOrSizing)
+end
+
+function LMIS:CreateIconTooltip(popup)
+	for _, btn in pairs(popup.IconSelector.ScrollBox:GetFrames()) do
+		btn:HookScript("OnEnter", LMIS.ShowTooltip)
+		btn:HookScript("OnLeave", LMIS.GameTooltip_Hide)
+	end
+	popup.BorderBox.SelectedIconArea.SelectedIconButton:HookScript("OnEnter", function(btn)
+		local fileid = btn:GetIconTexture()
+		self:SetIconTooltip(btn, function()
+			if fileid ~= 134400 then -- inv_misc_questionmark
+				GameTooltip:AddLine(format("|cff71D5FF%s|r", fileid))
+				if S.FileData[fileid] then
+					GameTooltip:AddLine(S.FileData[fileid], 1, 1, 1)
+				end
+			end
+		end)
 	end)
 end
 
